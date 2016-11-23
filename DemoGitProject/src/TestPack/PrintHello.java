28c1630d839484b0525d75e5a793c487cdf1360c
TP00360881 <TP00360881@INNOSZPC00056.ATT.TechMahindra.com>
2016-11-23 20:01:35 +0530

    defect#42525 in PrintHello file

--- DemoGitProject/src/TestPack/PrintHello.java
+++ DemoGitProject/src/TestPack/PrintHello.java
@@ -7,6 +7,11 @@ public class PrintHello {
 		System.out.println("Hello world");
 		System.out.println("This is first Git hub prohject");
 		System.out.println("This is line 2 for fix Upper case Messgae");
+		
+		System.out.println("Defect is fixed in this branch.Learning Git Hub");
+		
+		GreetTest gt=new GreetTest();
+		gt.greeting();
 	}
 
 }
