4bd665c1b87735a5b4cb92b387e31d66c57b89ae
TP00360881 <TP00360881@INNOSZPC00056.ATT.TechMahindra.com>
2016-11-23 18:30:05 +0530

    this is first commit

---
+++ DemoGitProject/src/TestPack/PrintHello.java
@@ -0,0 +1,11 @@
+package TestPack;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("This is first Git hub prohject");
+	}
+
+}
